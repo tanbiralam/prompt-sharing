c036d271e4bd02d19f52703fa88c6f31b4797bb5
Tanbir Alam <itzmetanbir@gmail.com>
2023-12-26 23:35:57 +0530

    Create Prompt Done

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+"use client"
+
+import {useState, useEffect} from 'react'
+
+import PromptCard from './PromptCard'
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState("")
+  const handleSearchChange = (e) => {
+
+  }
+  
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for prompts...'
+          value={searchText}
+          onChange={handleSearchChange}
+          className='search_input peer'
+        />
+      </form>
+    </section>
   )
 }
 
