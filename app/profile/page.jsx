c7c3d73d89e864fac85fc648ace805d6c25aa3a5
Tanbir Alam <itzmetanbir@gmail.com>
2023-12-29 00:41:19 +0530

    DELETE is not working properly

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -23,11 +23,28 @@ const MyProfile = () => {
   }, []);
 
   const handleEdit = async (post) => {
-    router.push(`/update-prompt?id=${post._id}`)
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
 
-  const handleDelete = (post) => {};
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -45,6 +45,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
+            alt="tick_image"
             width={12}
             height={12}
           />
@@ -56,7 +57,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
 
       {session?.user.id === post.creator._id && pathName === "/profile" && (
